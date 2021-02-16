d54c4a19a6cf471bbbe2c095487926dd28517871
matthieu.moret <matthieu.moret@thalesgroup.com>
2021-02-16 12:00:10 +0100

    ajout client share

--- leafcoin.sol
+++ leafcoin.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.1;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/presets/ERC20PresetMinterPauser.sol";
@@ -8,27 +10,29 @@ contract Leafcoin is ERC20PresetMinterPauser, Ownable   {
    
     
     struct Factory {
-        uint _totalShares;
+        uint totalShares;
+        uint clientShares;
         address[] payees;
         uint256[] shares;
-
+ 
     }
     
     
     mapping(address => Factory) public factories;
-    
-    constructor () public ERC20PresetMinterPauser ("Leafcoin", "LFC") {
-      //  _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+
+    constructor () ERC20PresetMinterPauser ("Leafcoin", "LFC") {
+        // By construction, there is initially ZERO leafcoins minted
     }
     
     
      /*
      * @dev Add a new factory product chain. (1 eth adress /product)
-     * @param factoryAddress The eth address of the factory
-     * @param payees Array of payee eth adressess
-     * @param shares Array of shares (same size)
+     * @param _address The eth address of the factory (a simple key for storage, no manipulation neither lfc nor eth are send)
+     * @param _payees Array of payee eth adressess
+     * @param _shares Array of shares (same size)
+     * @param _clientShares specifies client shares (address may vary for each product sell that triggers the mint trtansaction)
      */
-    function setupFactory (address _address, address[] memory _payees, uint256[] memory _shares)  public onlyOwner
+    function setupFactory (address _address, address[] memory _payees, uint256[] memory _shares, uint256 _clientShares)  public onlyOwner
     {
        
         require(_payees.length == _shares.length, "setupFactory: payees and shares length mismatch");
@@ -36,36 +40,50 @@ contract Leafcoin is ERC20PresetMinterPauser, Ownable   {
     
            
        // factories[_address].payees = new Payee[](_payees.length); //= Filliere( new Payee[](_payees.length), 0);
-        factories[_address]._totalShares = 0;
+        factories[_address].totalShares = 0;
         factories[_address].payees = _payees; 
         factories[_address].shares = _shares; 
         
         for (uint256 i = 0; i < _payees.length; i++) 
         {
-            factories[_address]._totalShares += _shares[i];
+            factories[_address].totalShares += _shares[i];
         }
         
+        // La part du client est stockée
+        factories[_address].totalShares += _clientShares;
+        factories[_address].clientShares = _clientShares; 
        
     }
     
     
      /**
-     * @dev Triggers a transfer to `account` of the amount of LFC they are owed, according to their percentage of the
-     * total shares and their previous withdrawals.
+     * @dev Triggers a transfer to factory accounts of the amount of LFC they are owed, according to their shares defined by setupFactory
+     * Client address might be left to 0, thus transfering leafcoins to owner (for R&D purpose)
+     * @param _address sharesThe eth address of the factory (a simple key for storage, no manipulation)
+     * @param _amount amount of leacoin to generate for this trtansaction
+     * @param _client client shares will be credited with factory defined _clientShared (can be left to 0)
      */
-    function mymint(address _address, uint256 amount) public onlyOwner
+    function mymint(address _address, uint256 _amount, address _client) public onlyOwner
     {
-        require(factories[_address]._totalShares > 0, "mymint: account is not a factory");
+        require(factories[_address].totalShares > 0, "mymint: account is not a factory");
+        require(_amount > 0, "mymint: amount is NULL");
 
         // Mine amount locally
-         _mint(msg.sender, amount);
+         _mint(msg.sender, _amount);
         
-        // For each payee of the ffactory contract transfer leafcoins according to shares
+        Factory memory factory = factories[_address];
+        
+        // For each payee of the factory contract transfer leafcoins according to shares
         for (uint i = 0; i < factories[_address].payees.length; i++) 
         {
-            uint256 amountTo = amount * factories[_address].shares[i] / factories[_address]._totalShares;
-            transfer(factories[_address].payees[i], amountTo );
+            uint256 amountTo = _amount * factory.shares[i] / factory.totalShares;
+            transfer(factory.payees[i], amountTo );
             
         }
+        
+        // The client shares are transfered id address specified; if not leave leafcoins on owner address
+        uint256 clientAmount = _amount * factories[_address].clientShares / factories[_address].totalShares ;
+        if (_client != address(0))
+            transfer(_client, clientAmount);
     }
 }
